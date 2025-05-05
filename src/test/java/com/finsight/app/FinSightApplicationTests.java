5fe4e71d1e962b5cbb9d8cbd1d72780ba82abb09
Nachiket More <nachiketmore.more@gmail.com>
2025-05-05 11:30:00 -0700

    Initial Spring Boot project setup with dependencies

---
+++ src/test/java/com/finsight/app/FinSightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finsight.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinSightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
